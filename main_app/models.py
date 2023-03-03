ccff5cc13b4ad7e2822d59d9e779d623478c686d
EricCSheppard <EricCSheppard@gmail.com>
2023-03-03 10:25:49 -0500

    part 2 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    genus = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    common_name = models.CharField(max_length=100)
+    distribution = models.TextField(max_length=250)
+    img_url = models.TextField(max_length=250)
+    def __str__(self):
+        return self.common_name
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import Finch
+# finches = [
+#     {'genus': 'Mycerobas', 'scientific_name': 'Mycerobas affinis', 'common_name': 'Collared grosbeak', 'distribution': 'Bhutan, India, Myanmar, Nepal and Thailand'},
+#     {'genus': 'Mycerobas', 'scientific_name': 'Mycerobas carnipes', 'common_name': 'White-winged grosbeak', 'distribution': 'Afghanistan, Bhutan, China, India, Iran, Myanmar, Nepal, Pakistan, Russia, Tajikistan, Turkmenistan, and Uzbekistan'}
+# ]
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finch_index(request):
+    # just like we passed data to our templates in express
+    # we pass data to our templates trough our view functions
+    # we can gather relations from SQL using our model methods
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', { 'finches': finches })
+
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch})
